0f5b8fb7d6a4c2808d8ceeeeb3f138a2685e72e2
Borislav Grigorov <bobi@noblehire.io>
2021-01-21 17:10:28 +0200

    refactor gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,49 @@
-const { series, watch, src, dest, task } = require("gulp");
+const {parallel, series, watch, src, dest, task} = require("gulp");
 const sass = require("gulp-sass");
 const del = require("del");
+const cssPath = "./public/css";
+const clientJsPath = "./public/js";
+const imagesPath = "./public/images";
 
-function clean(cb) {
-  return del(["./public/css/**"], cb);
+function clean(path) {
+  return function cleanGivenPath(cb) {
+    return del([path], cb);
+  }
 }
 
 function css() {
   return src("./src/**/*.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(dest("./public/css"));
+    .pipe(dest(cssPath));
+}
+
+function images() {
+  return src("./src/images/**").pipe(dest(imagesPath));
 }
 
 function clientJs() {
   return src("./src/client.js")
-    .pipe(dest("./public/js"));
+    .pipe(dest(clientJsPath));
 }
 
-module.exports = {
-  cssbuild: task("cssbuild", series(clean, css)),
-  css: task("css", () => watch(["./src/**/*.scss"], series(clean, css))),
-  clientjsbuild: task("clientjsbuild", series(clientJs)),
-  clientjs: task("clientjs", () => watch(["./src/client.js"], series(clientJs))),
+const tasks = {
+  // dev/watch tasks
+  css: task("css", () => watch(["./src/**/*.scss"], series(clean(cssPath), css))),
+  clientJs: task("clientJs", () => watch(["./src/client.js"], series(clean(clientJsPath), clientJs))),
+  images: task("images", () => watch(["./src/images/**"]), series(clean(imagesPath), images)),
+
+  // runs a parallel task with all the above
+  watch: task('watch', parallel('css', 'clientJs', 'images')),
+
+  // THE build task
+  build: task("build", series(
+    clean(cssPath),
+    css,
+    clean(clientJsPath),
+    clientJs,
+    clean(imagesPath),
+    images,
+  )),
 };
+
+module.exports = tasks;
