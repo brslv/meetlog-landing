adcd9bafa95dee3a1809c97bef2c3d7c11b8717c
Borislav Grigorov <bobi@noblehire.io>
2021-01-28 10:42:39 +0200

    fix pg issues

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const path = require("path");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 (async function () {
-  dotenv.config({path: path.join(__dirname, "../.env")});
+  dotenv.config({ path: path.join(__dirname, "../.env") });
 
-  const {Client} = require('pg')
-  const client = new Client()
-  await client.connect()
-// console.log(res.rows[0].message) // Hello world!
+  const { Client } = require("pg").native;
+  const client = new Client();
+  client.on("error", (error) => {
+    console.log("DB Client error occurred:\n");
+    console.log(error);
+  });
+  await client.connect();
+  // console.log(res.rows[0].message) // Hello world!
 
   app.use(express.static("public"));
   app.use(express.json());
@@ -24,18 +28,25 @@ const port = 3000;
     res.set("content-type", "application/json");
 
     try {
-      await client.query('INSERT INTO subs(email, get_updates) VALUES($1, $2);', [data.email, data.getUpdates]);
-      const countResult = await client.query('SELECT COUNT(*) FROM subs;');
+      await client.query(
+        "INSERT INTO subs(email, get_updates) VALUES($1, $2);",
+        [data.email, data.getUpdates]
+      );
+      const countResult = await client.query("SELECT COUNT(*) FROM subs;");
       const count = countResult.rows[0].count;
-      return res.send({ok: true, count: (!count || count < 21) ? 21 : count }); // 😇
+      return res.send({ ok: true, count: !count || count < 21 ? 21 : count }); // 😇
     } catch (e) {
       const duplicateEmailErrCode = 23505;
       if (duplicateEmailErrCode === Number(e.code))
-        return res.send({ok: false, message: "You're already on the list. 😻"});
+        return res.send({
+          ok: false,
+          message: "You're already on the list. 😻",
+        });
       return res.send({
         ok: false,
-        message: "Oops, something unexpected happened and I couldn't add you to the list... Yell at me on twitter/email to sort that out, please!"
-      })
+        message:
+          "Oops, something unexpected happened and I couldn't add you to the list... Yell at me on twitter/email to sort that out, please!",
+      });
     }
   });
 
